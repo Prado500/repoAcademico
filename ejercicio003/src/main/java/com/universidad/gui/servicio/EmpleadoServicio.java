9433e38b0ce22fbac73f47b17568e4312ee64847
Prado500 <daldeosmail@gmail.com>
2025-08-20 22:28:29 -0500

    Creacion del metodo generico para la familia Empleado searchEmpleadoPorNoDocumento en el EmpleadoServicio

--- ejercicio003/src/main/java/com/universidad/gui/servicio/EmpleadoServicio.java
+++ ejercicio003/src/main/java/com/universidad/gui/servicio/EmpleadoServicio.java
@@ -11,26 +11,37 @@ import java.util.List;
 /**
  *
  * @author Alejandro
- * 
+ *
  * Servicio para gestionar objetos de la familia Empleado
- * 
+ *
  * @param <T> el tipo de empleado que debe extender de Empleado
- * 
+ *
  */
-public class EmpleadoServicio <T extends Empleado> {
+public class EmpleadoServicio<T extends Empleado> {
 
-    private List<T> elementos; 
-    
-    public EmpleadoServicio(){
-    this.elementos = new ArrayList<>();
-    
+    private List<T> elementos;
+
+    public EmpleadoServicio() {
+        this.elementos = new ArrayList<>();
+
     }
 
     public void agregar(T elemento) {
         elementos.add(elemento);
     }
-    
-    public List<T> mostrar(List<T> elementos){
-        return new ArrayList<>(elementos); 
+
+    public List<T> mostrar(List<T> elementos) {
+        return new ArrayList<>(elementos);
+    }
+
+    public T searchElementoByNoDocumento(String noDocumento) {
+        T elementoRetorno = null;
+        for (T elemento : elementos) {
+            if (elemento.getNoDoumento().equals(noDocumento)) {
+                elementoRetorno = elemento;
+            }
+
+        }
+        return elementoRetorno;
     }
 }
