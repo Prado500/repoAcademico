e3e7fee43e36043fb47e1cc1fccc6949b5cdeea7
Prado500 <daldeosmail@gmail.com>
2025-09-22 10:05:08 -0500

    Added function verificarfechaCaducidad to verify wether the input value of attribute fechaCaducidad is valid or not

--- ejercicio003/src/main/java/com/universidad/gui/modelo/implementacion/Comanda.java
+++ ejercicio003/src/main/java/com/universidad/gui/modelo/implementacion/Comanda.java
@@ -4,7 +4,11 @@
  */
 package com.universidad.gui.modelo.implementacion;
 
-import java.util.regex.Matcher;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 /**
@@ -22,8 +26,7 @@ public class Comanda {
     private String fechaCaducidad;
     public static final Pattern PATRON_CARACTERES_PERMITIDOS_DESCRIPCION = Pattern.compile("\"^[A-Za-zÁÉÍÓÚáéíóúÜüÑñ\\\\s]{1,204}$\""); // Expresión regular que genera un patrón de búsqueda para verificar si la entrada de los campos que se escribiran manualmente (descripcion, principio, proteina y sopa) esta conformada unica y exclusivamente por combinaciones de letras y vocales del alfabeto del español latino.
     public static final Pattern PATRON_CARACTERES_PERMITIDOS = Pattern.compile("^[A-Za-zÁÉÍÓÚáéíóúÜüÑñ\\\\s]{1,50}$"); // Expresión regular que genera un patrón de búsqueda para verificar si la entrada de los campos que se escribiran manualmente (descripcion, principio, proteina y sopa) esta conformada unica y exclusivamente por combinaciones de letras y vocales del alfabeto del español latino.
-    public static final Pattern PARTRON_VERIFICAR_FECHA_CADUCIDAD = Pattern.compile();
-            
+
     // Constructor
     public Comanda(String descripcion, String principio, String proteina,
             String sopa, String fechaCaducidad) {
@@ -36,7 +39,7 @@ public class Comanda {
         this.proteina = proteina;
         verificarInformacion(sopa);
         this.sopa = sopa;
-        
+        verificarFechaCaducidad(fechaCaducidad);
         this.fechaCaducidad = fechaCaducidad;
     }
 
@@ -107,31 +110,67 @@ public class Comanda {
         return id += 1;
     }
 
-    private void verificarInformacion(String input){
-     
-    if(descripcion.isBlank() || ! PATRON_CARACTERES_PERMITIDOS.matcher(input).matches()){
-        throw new IllegalArgumentException("""
+    private void verificarInformacion(String input) {
+
+        if (descripcion.isBlank() || !PATRON_CARACTERES_PERMITIDOS.matcher(input).matches()) {
+            throw new IllegalArgumentException("""
                                            Descripción inválida. Ingrese en la descripción palabras
                                            y oraciones compuestas únicamente por combinaciones
                                            entre las letras y vocales del alfabeto español lationamericano.
                                            """);
+        }
     }
-    }
-    
-   
-     private void verificarInformacionDescripcion(String input){
-     
-    if(descripcion.isBlank() || ! PATRON_CARACTERES_PERMITIDOS_DESCRIPCION.matcher(input).matches()){
-        throw new IllegalArgumentException("""
+
+    private void verificarInformacionDescripcion(String input) {
+
+        if (descripcion.isBlank() || !PATRON_CARACTERES_PERMITIDOS_DESCRIPCION.matcher(input).matches()) {
+            throw new IllegalArgumentException("""
                                            Descripción inválida. Ingrese en la descripción palabras
                                            y oraciones compuestas únicamente por combinaciones
                                            entre las letras y vocales del alfabeto español lationamericano.
                                            """);
+        }
     }
+
+    private void verificarFechaCaducidad(String fechaCaducidad) {
+
+        Objects.requireNonNull(fechaCaducidad, "La fecha de caducidad de la comanda no puede ser nula.");
+
+        if (!fechaCaducidad.matches("\\d{2}/\\d{2}/\\d{4}/")) {
+
+            throw new IllegalArgumentException("""
+                                                Formato de fecha inválido. Ingrese una fecha de acuerdo al formato
+                                                DD/MM/AAAA
+                                                """);
+
+        }
+        
+        try{
+        
+            DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-uuuu").withResolverStyle(ResolverStyle.STRICT);
+            
+            LocalDate fecha = LocalDate.parse(this.fechaCaducidad, formato);
+            
+            LocalDate minFechaCaducidad = LocalDate.now();
+            
+            LocalDate maxFechaCaducidad = LocalDate.now().plusDays(15);
+            
+            if (fecha.isBefore(minFechaCaducidad) || fecha.isAfter(maxFechaCaducidad)){
+                throw new IllegalArgumentException("""
+                                                   Error de digitación de la fecha de caducidad. Recuerde que la fecha de
+                                                   caducidad no puede ser mas antigua a la del día de hoy y no puede extenderse
+                                                   mas de 15 días.
+                                                   """);
+            }
+        
+        
+        }catch(DateTimeParseException e){
+            throw new IllegalArgumentException("""
+                                               Error de digitación de la fecha. Ingrese una fecha de acuerdo al formato
+                                               DD/MM/AAAA
+                                               """ + e.getMessage());
+        
+        }
+
     }
-     
-    
-     private void verificarFechaCaducidad(){
-     
-     }
 }
